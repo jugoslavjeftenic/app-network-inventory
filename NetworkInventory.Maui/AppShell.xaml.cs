aac4dd0ed41700f1be162d07f62129e0da4f6907
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-05 18:22:56 +0200

    Routing

--- NetworkInventory.Maui/AppShell.xaml.cs
+++ NetworkInventory.Maui/AppShell.xaml.cs
@@ -1,9 +1,15 @@
-﻿namespace NetworkInventory.Maui;
+﻿using NetworkInventory.Maui.Views;
+
+namespace NetworkInventory.Maui;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(DevicesPage), typeof(DevicesPage));
+		Routing.RegisterRoute(nameof(AddDevicePage), typeof(AddDevicePage));
+		Routing.RegisterRoute(nameof(EditDevicePage), typeof(EditDevicePage));
 	}
 }
