a1b2b03c751a3d7327a49b70826ffa956516b31b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-05 10:41:13 +0200

    Add project files.

---
+++ NetworkInventory.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NetworkInventory.Maui
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
