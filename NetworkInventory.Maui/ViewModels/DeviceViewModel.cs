4bb317bd8888e1176f9bb94cae9a634fd3fb499b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-21 14:39:04 +0200

    Device ViewModel

---
+++ NetworkInventory.Maui/ViewModels/DeviceViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NetworkInventory.Maui.Models;
+using Device = NetworkInventory.Maui.Models.Device;
+
+namespace NetworkInventory.Maui.ViewModels;
+
+public partial class DeviceViewModel : ObservableObject
+{
+	private Device? _device;
+	public Device? Device
+	{
+		get => _device;
+		set
+		{
+			SetProperty(ref _device, value);
+		}
+	}
+
+	public DeviceViewModel()
+	{
+		Device = new();
+	}
+
+	public void LoadDevice(int deviceId)
+	{
+		Device = DevicesRepository.GetDeviceById(deviceId);
+	}
+
+	[RelayCommand]
+	public void SaveDevice()
+	{
+		if (Device is not null)
+		{
+			DevicesRepository.UpdateDevice(Device.Id, Device);
+		}
+	}
+}
