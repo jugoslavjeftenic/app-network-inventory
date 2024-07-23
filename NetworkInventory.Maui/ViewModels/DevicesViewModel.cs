222eb6b1db8eba08bab389b859d9d0e5017f530d
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-23 10:18:23 +0200

    Devices ViewModel

---
+++ NetworkInventory.Maui/ViewModels/DevicesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using NetworkInventory.UseCases.Interfaces;
+using System.Collections.ObjectModel;
+using Device = NetworkInventory.CoreBusiness.Device;
+
+namespace NetworkInventory.Maui.ViewModels;
+
+public class DevicesViewModel(IViewDevicesUseCase viewDevicesUseCase)
+{
+	private readonly IViewDevicesUseCase _viewDevicesUseCase = viewDevicesUseCase;
+
+	public ObservableCollection<Device> Devices { get; set; } = [];
+
+	public async Task LoadDevicesAsync()
+	{
+		Devices.Clear();
+
+		var devices = await _viewDevicesUseCase.ExecuteAsync(string.Empty);
+		if (devices.Count > 0)
+		{
+			foreach (var device in devices)
+			{
+				Devices.Add(device);
+			}
+		}
+	}
+}
