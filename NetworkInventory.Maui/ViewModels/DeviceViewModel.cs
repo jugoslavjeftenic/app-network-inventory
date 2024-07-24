376235370f2b8ba9fdd903e9e9a94588f3284305
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-24 20:02:26 +0200

    Refactoring for new Device model

--- NetworkInventory.Maui/ViewModels/DeviceViewModel.cs
+++ NetworkInventory.Maui/ViewModels/DeviceViewModel.cs
@@ -1,6 +1,4 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using NetworkInventory.Maui.Models;
 using NetworkInventory.UseCases.Interfaces;
 using Device = NetworkInventory.CoreBusiness.Device;
 
