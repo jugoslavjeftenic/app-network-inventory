44d3f2314b52eca0b865e40412ecf83f62a51ce8
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-28 12:03:41 +0200

    Cleanup commented code

--- NetworkInventory.Maui/Views/AddDevicePage.xaml.cs
+++ NetworkInventory.Maui/Views/AddDevicePage.xaml.cs
@@ -20,43 +20,4 @@ public partial class AddDevicePage : ContentPage
 
 		_deviceViewModel.Device = new CoreBusiness.Device();
 	}
-
-	//private readonly IAddDeviceUseCase _addDeviceUseCase;
-
-	//public AddDevicePage(IAddDeviceUseCase addDeviceUseCase)
-	//{
-	//	InitializeComponent();
-	//	_addDeviceUseCase = addDeviceUseCase;
-	//}
-
-	//private async void DeviceControl_OnSave(object sender, EventArgs e)
-	//{
-	//	await _addDeviceUseCase.ExecuteAsync(new Device
-	//	{
-	//		Name = DeviceControl.Name ?? "",
-	//		SerialNumber = DeviceControl.SerialNumber ?? "",
-	//		IPv4 = DeviceControl.IPv4 ?? "",
-	//		SubnetMask = DeviceControl.SubnetMask ?? "",
-	//		Gateway = DeviceControl.Gateway ?? "",
-	//		PreferredDNS = DeviceControl.PreferredDNS ?? "",
-	//		AlternateDNS = DeviceControl.AlternateDNS ?? "",
-	//		Vlan = DeviceControl.Vlan ?? "",
-	//		UpstreamDevice = DeviceControl.UpstreamDevice ?? "",
-	//		Location = DeviceControl.Location ?? "",
-	//		User = DeviceControl.User ?? "",
-	//	});
-
-	//	await Shell.Current.GoToAsync("..");
-	//}
-
-	//private void DeviceControl_OnCancel(object sender, EventArgs e)
-	//{
-	//	Shell.Current.GoToAsync("..");
-	//}
-
-	//private void DeviceControl_OnError(object sender, string e)
-	//{
-	//	var toast = Toast.Make(e, ToastDuration.Short);
-	//	toast.Show();
-	//}
 }
